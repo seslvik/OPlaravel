6588b268ad6547b07089274f4e4589f1efb9275d
seslvik <seslvik@mail.ru>
2020-08-13 00:26:33 +0300

    Добавил firstname при регистрации и его редактирование

---
+++ resources/views/includes/styles_and_js.blade.php
@@ -0,0 +1,14 @@
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('css/icons.css') }}" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('css/style.css') }}" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('css/jquery-ui.css') }}" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('css/leaflet.css') }}" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('css/morris/morris.css') }}" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('css/datatables/dataTables.bootstrap4.min.css') }}" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('css/datatables/buttons.bootstrap4.min.css') }}" rel="stylesheet" type="text/css" />
+    {{--<link href="{{ asset('css/notifIt.js') }}" rel="stylesheet" type="text/css" />--}}
+    <script type="text/javascript" src="{{ asset('js/jquery-1.12.4.min.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('js/jquery-ui.min.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('js/imgViewer2.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('js/leaflet.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('js/bs-custom-file-input.js') }}"></script> {{--<script type="text/javascript" src="{{ asset('js/notifIt.min.js') }}"></script>--}}
